e8ce4f9bc17bf54df00ece6a4c432c0079d14203
joshjennings98 <jenninjl01@gmail.com>
2019-11-22 12:04:44 +0000

    Fixed shell script and sorted python files

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
-python3 test_generator.py
+python3 test_network.py
 
 graph_schema-4.2.0/tools/compile_graph_as_provider.sh test_network.xml
 
-mv -f providers
+mv -f test_network.graph.so graph_schema-4.2.0/providers
 
-graph_schema-4.2.0/bin/epoch_sim test_network.xml
+cd graph_schema-4.2.0
+
+bin/epoch_sim /home/josh/fyp/test_network.xml
+
+cd ..
 

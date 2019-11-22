b4fe23dd6d6393495c5002f5822b9302b5d16350
joshjennings98 <jenninjl01@gmail.com>
2019-11-22 14:28:30 +0000

    Improved string formatting and removed unnecesarry code.

--- test_generator.py
+++ test_generator.py
@@ -8,120 +8,111 @@ class Neuron(object):
 
 def makeGraph(neurons, name, maxt):
     deviceInstances = []
-    edgeInstances = []
+    edgeInstances = []        
 
     for neuron in neurons:
-        device = "   <DevI id=\"" + neuron.name + "\" type=\"neuron\"><P>\"fire_init\":" + str(neuron.fire_init) + "</P></DevI>\n"
+        device = "  <DevI id=\"%s\" type=\"neuron\"><P>\"fire_init\":%i</P></DevI>\n" % (neuron.name, neuron.fire_init)
         deviceInstances.append(device)
-
-    num_neurons = len(deviceInstances)
-
-    for neuron in neurons:
         connections = []
         for connection in range(len(neuron.connections)): # change so only does connections set to 1
             if neuron.connections[connection] == 1: 
                 weight = 1.0 # change to random value
-                x = "   <EdgeI path=\"" + neurons[connection].name + ":input-" + neuron.name + ":fire\"><P>\"weight\":" + str(weight) + "</P></EdgeI>\n" 
-                connections.append(x)
-        y = "".join(connections)
-        edgeInstances.append(y)
+                edge = "   <EdgeI path=\"%s:input-%s:fire\"><P>\"weight\":%s</P></EdgeI>\n" % (neurons[connection].name, neuron.name, weight)
+                connections.append(edge)
+        edgeInstances.append("".join(connections))
 
-    # To Do: Make this thing nicer using multiline strings or something
-    graph = [
-        "<?xml version='1.0'?>\n",
-        "<Graphs xmlns=\"https://poets-project.org/schemas/virtual-graph-schema-v3\">\n",
-        "<GraphType xmlns=\"https://poets-project.org/schemas/virtual-graph-schema-v3\" id=\"", name, "\"> \n",
-        "  <Properties> \n",
-        "    <Scalar name=\"max_t\" type=\"uint32_t\"/> \n",
-        "	<Scalar name=\"neuron_count\" type=\"uint32_t\"/> \n",
-        "  </Properties> \n",
-        "  <SharedCode><![CDATA[ \n",
-        "    #ifdef POETS_LEGACY_HAS_HANDLER_EXIT \n",
-        "    #define _do_handler_exit(code) handler_exit(code) \n",
-        "    #else \n",
-        "    #define _do_handler_exit(code) ((void)0) \n",
-        "    #endif \n",
-        " \n",
-        "    #define fake_handler_exit(code) \\\n",
-        "    { \\\n",
-        "        if((code)==0){ \\\n",
-        "            handler_log(0, \"_HANDLER_EXIT_SUCCESS_9be65737_\"); \\\n",
-        "        }else{ \\\n",
-        "            handler_log(0, \"_HANDLER_EXIT_FAIL_9be65737_\"); \\\n",
-        "        } \\\n",
-        "        _do_handler_exit(code); \\\n",
-        "    } \n",
-        "    ]]></SharedCode> \n",
-        "  <MessageTypes> \n",
-        "    <MessageType id=\"synapse\"> \n",
-        "      <Message> \n",
-        "        <Scalar name=\"fired\" type=\"int8_t\"/> \n",
-        "      </Message> \n",
-        "    </MessageType> \n",
-        "  </MessageTypes> \n",
-        "  <DeviceTypes> \n",
-        "    <DeviceType id=\"neuron\"> \n",
-        "      <Properties> \n",
-        "        <Scalar name=\"seed\" type=\"uint32_t\"/> \n",
-        "        <Scalar name=\"fire_init\" type=\"int8_t\" default=\"0\"/> \n",
-        "      </Properties> \n",
-        "      <State> \n",
-        "        <Scalar name=\"fireValue\" type=\"int8_t\"/> \n",
-        "        <Scalar name=\"rts\" type=\"uint32_t\"/> \n",
-        "        <Scalar name=\"t\" type=\"uint32_t\"/> \n",
-        "      </State> \n",
-        "      <OnInit><![CDATA[    \n",
-        "        if (deviceProperties->fire_init == 1) { \n",
-        "          handler_log(1, \"Fired Spike\"); \n",
-        "          deviceState->fireValue = true; \n",
-        "        } else { \n",
-        "          deviceState->fireValue = false; \n",
-        "        }		   \n",
-        "		  ]]></OnInit> \n",
-        "      <InputPin name=\"input\" messageTypeId=\"synapse\"> \n",
-        "        <Properties> \n",
-        "          <Scalar name=\"weight\" type=\"float\"/> \n",
-        "        </Properties> \n",
-        "        <OnReceive><![CDATA[ \n",
-        "		      if(message->fired){ \n",
-        "            handler_log(1, \"Recieved Spike\"); \n",
-        "            deviceState->fireValue = true; \n",
-        "		      } \n",
-        "          deviceState->rts = RTS_FLAG_fire; \n",
-        "		  ]]></OnReceive> \n",
-        "      </InputPin> \n",
-        "      <OutputPin name=\"fire\" messageTypeId=\"synapse\" indexed=\"false\"> \n",
-        "        <OnSend><![CDATA[ \n",
-        "		      message->fired=deviceState->fireValue; // Add conditional in future for v_threshold \n",
-        "          handler_log(1, \"Fired Spike\"); \n",
-        "          deviceState->fireValue = false; \n",
-        "          deviceState->t++; \n",
-        "	        if(deviceState->t > graphProperties->max_t / graphProperties->neuron_count){ \n",
-        "		        *doSend=0; \n",
-        "			      fake_handler_exit(0); \n",
-        "	      	} \n",
-        "		  ]]></OnSend> \n",
-        "      </OutputPin> \n",
-        "      <ReadyToSend><![CDATA[ \n",
-        "		    *readyToSend = (deviceState->fireValue == true) ? RTS_FLAG_fire : 0; \n",
-        "		]]></ReadyToSend> \n",
-        "    </DeviceType> \n",
-        "  </DeviceTypes> \n",
-        "</GraphType> \n",
-        " <GraphInstance id=\"", name, "_output", "\" graphTypeId=\"", name, "\"> \n",
-        "   <Properties> \n",
-        "    \"max_t\":", str(maxt), ",\n",
-        "    \"neuron_count\":", str(num_neurons), "\n",
-        "   </Properties> \n",
-        "  <DeviceInstances> \n",
-        "".join(deviceInstances),
-        "  </DeviceInstances> \n",
-        "  <EdgeInstances> \n",
-        "".join(edgeInstances),
-        "  </EdgeInstances> \n",
-        " </GraphInstance> \n",
-        "</Graphs> \n",
-    ]
+    graph = """<?xml version='1.0'?>
+<Graphs xmlns="https://poets-project.org/schemas/virtual-graph-schema-v3">
+<GraphType xmlns="https://poets-project.org/schemas/virtual-graph-schema-v3" id="%s"> 
+<Properties> 
+    <Scalar name="max_t" type="uint32_t"/> 
+    <Scalar name="neuron_count" type="uint32_t"/> 
+</Properties> 
+<SharedCode><![CDATA[ 
+    #ifdef POETS_LEGACY_HAS_HANDLER_EXIT 
+    #define _do_handler_exit(code) handler_exit(code) 
+    #else 
+    #define _do_handler_exit(code) ((void)0) 
+    #endif 
+
+    #define fake_handler_exit(code) \\
+    { \\
+        if((code)==0){ \\
+            handler_log(0, "_HANDLER_EXIT_SUCCESS_9be65737_"); \\
+        }else{ \\
+            handler_log(0, "_HANDLER_EXIT_FAIL_9be65737_"); \\
+        } \\
+        _do_handler_exit(code); \\
+    } 
+    ]]></SharedCode> 
+<MessageTypes> 
+    <MessageType id="synapse"> 
+    <Message> 
+        <Scalar name="fired" type="int8_t"/> 
+    </Message> 
+    </MessageType> 
+</MessageTypes> 
+<DeviceTypes> 
+    <DeviceType id="neuron"> 
+    <Properties> 
+        <Scalar name="seed" type="uint32_t"/> 
+        <Scalar name="fire_init" type="int8_t" default="0"/> 
+    </Properties> 
+    <State> 
+        <Scalar name="fireValue" type="int8_t"/> 
+        <Scalar name="rts" type="uint32_t"/> 
+        <Scalar name="t" type="uint32_t"/> 
+    </State> 
+    <OnInit><![CDATA[    
+        if (deviceProperties->fire_init == 1) { 
+        handler_log(1, "Fired Spike"); 
+        deviceState->fireValue = true; 
+        } else { 
+        deviceState->fireValue = false; 
+        }		   
+        ]]></OnInit> 
+    <InputPin name="input" messageTypeId="synapse"> 
+        <Properties> 
+        <Scalar name="weight" type="float"/> 
+        </Properties> 
+        <OnReceive><![CDATA[ 
+            if(message->fired){ 
+            handler_log(1, "Recieved Spike"); 
+            deviceState->fireValue = true; 
+            } 
+        deviceState->rts = RTS_FLAG_fire; 
+        ]]></OnReceive> 
+    </InputPin> 
+    <OutputPin name="fire" messageTypeId="synapse" indexed="false"> 
+        <OnSend><![CDATA[ 
+            message->fired=deviceState->fireValue; // Add conditional in future for v_threshold 
+        handler_log(1, "Fired Spike"); 
+        deviceState->fireValue = false; 
+        deviceState->t++; 
+            if(deviceState->t > graphProperties->max_t / graphProperties->neuron_count){ 
+                *doSend=0; 
+                fake_handler_exit(0); 
+            } 
+        ]]></OnSend> 
+    </OutputPin> 
+    <ReadyToSend><![CDATA[ 
+            *readyToSend = (deviceState->fireValue == true) ? RTS_FLAG_fire : 0; 
+        ]]></ReadyToSend> 
+    </DeviceType> 
+</DeviceTypes> 
+</GraphType> 
+<GraphInstance id="%s_output" graphTypeId="%s"> 
+<Properties> 
+    "max_t":%i,
+    "neuron_count":%i
+</Properties> 
+<DeviceInstances> 
+%s    </DeviceInstances> 
+<EdgeInstances> 
+%s    </EdgeInstances> 
+</GraphInstance> 
+</Graphs> 
+    """ % (name, name, name, maxt, len(deviceInstances), "".join(deviceInstances), "".join(edgeInstances))
 
     return graph
 
@@ -131,5 +122,5 @@ def saveGraph(graph, filename):
     file.writelines(graph) 
     file.close()
 
-    print("".join(graph))
+    print(graph)
     print("Graph saved as:", filename)

0b066545dd729f8e3328df9263a2b45b670267ba
Josh <jenninjl01@gmail.com>
2020-04-28 16:17:34 +0100

    Fixed seed to 123 on test_model.py

--- test_model.py
+++ test_model.py
@@ -3,14 +3,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(8758956)
+np.random.seed(123)
 
 Ne = 800
 Ni = 200
 
 epochs = 1000
 
-"""
 re = np.random.uniform(0, 1, Ne)
 ri = np.random.uniform(0, 1, Ni)
 
@@ -23,20 +22,6 @@ S = np.concatenate([0.5 * np.random.uniform(0, 1, (Ne, Ne + Ni)), -1 * np.random
 
 v = -65 * np.ones(Ne + Ni)
 u = b * v
-"""
-
-re = np.random.uniform(0.8, 1, Ne)
-ri = np.random.uniform(0.8, 1, Ni)
-
-a = np.concatenate([0.02 * np.ones(Ne), 0.1 * ri])
-b = np.concatenate([0.2 * np.ones(Ne), 0.2 * ri])
-c = np.concatenate([-65 * re, -65 * np.ones(Ni)])
-d = np.concatenate([2 * re, 2 * np.ones(Ni)])
-
-S = np.concatenate([0.5 * np.random.uniform(0, 1, (Ne, Ne + Ni)), -1 * np.random.uniform(0, 1, (Ni, Ne + Ni))])
-
-v = a
-u = b
 
 firings = []
 
@@ -51,11 +36,12 @@ for t in range(epochs):
 
     I += np.sum(S[fired],axis=0)
 
-    v = v + 0.5 * (0.04 * v * v + 5 * v + 140 - u + I)  # step 0.5 ms
-    v = v + 0.5 * (0.04 * v * v + 5 * v + 140 - u + I)  # for numerical
+    v = v + 1 * (0.04 * v * v + 5 * v + 140 - u + I)  # step 0.5 ms
+    #v = v + 0.5 * (0.04 * v * v + 5 * v + 140 - u + I)  # for numerical
 
     u = u + a * (b * v - u)                                    # stability
 
+"""
 fig, (axis1, axis2) = plt.subplots(1, 2)
 fig.suptitle("Plot of which neurons are firing at each epoch")
 
@@ -81,5 +67,38 @@ axis2.set_ylim(0, Ne+Ni)
 axis2.set_xlabel("Epoch")
 axis2.set_ylabel("Neuron")
 axis2.set_title("When each neuron fires")
+
+plt.show()
+"""
+
+fig, axis = plt.subplots(1, 1)
+fig.suptitle("Plot of which neurons are firing at each epoch")
+
+axis.plot(range(epochs), list(map(lambda l: len(l), firings)))
+axis.set_xlim(0, epochs)
+axis.set_ylim(0, Ne+Ni)
+
+axis.set_xlabel("Epoch")
+axis.set_ylabel("Number of firing neurons")
+axis.set_title("Quantity of neurons firing")
+
+plt.show()
+
+fig, axis = plt.subplots(1, 1)
+
+firings = list(zip(range(epochs), firings))
+firings = list(map(lambda t: list(map(lambda x: (x, t[0]), t[1])), firings))
+firings = list(filter(lambda l: l != [], firings))
+firings = [j for i in firings for j in i]
+
+ydata, xdata = zip(*firings)
+
+axis.scatter(xdata, ydata, s=1)
+axis.set_xlim(0, epochs)
+axis.set_ylim(0, Ne+Ni)
+
+axis.set_xlabel("Epoch")
+axis.set_ylabel("Neuron")
+axis.set_title("When each neuron fires")
 
 plt.show()

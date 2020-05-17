4b79fb42a81f1df7ef39e1b481df8633c1efdbfe
Josh <jenninjl01@gmail.com>
2020-05-17 20:07:21 +0100

    removed broken weight thing from generator

--- network_generator/neuronGenerator.py
+++ network_generator/neuronGenerator.py
@@ -28,7 +28,6 @@ def genConnections(num : int, params : List[Tuple[List[str], float, float, float
     i = 0
     for param in params:
         connectionProb = param[3]
-        weight = param[4]
         numOfThisNeuron = param[1]
 
         for _ in range(int(num * numOfThisNeuron)):
@@ -44,7 +43,7 @@ def genConnections(num : int, params : List[Tuple[List[str], float, float, float
             else:
                 connections = [1 for k in range(num)]
                     
-            neuron = NeuronConnections(name, connections, weight)
+            neuron = NeuronConnections(name, connections)
             yield neuron
             i += 1 
 
